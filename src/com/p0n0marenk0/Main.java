f3da50b763ef45f0cc10646d5a6f93154ea79567
Serhii Ponomarenko <ponomarenkoss99@gmail.com>
2021-02-26 22:38:00 +0200

    add task1 with compound interest to new class Task81.java and format code

--- src/com/p0n0marenk0/Main.java
+++ src/com/p0n0marenk0/Main.java
@@ -1,25 +1,8 @@
-//task1
-//в банк положили S денег.
-//Какой станет сумма вклада через N лет,
-//если процент 1,5% добавляется к сумме вклада ежемесячно.
 package com.p0n0marenk0;
-
-import java.util.Scanner;
-
 public class Main {
-
-    public static final int amountOfMonthsInYear = 12;
-    public static final double percent15 = 1.5;
+    private static Object Task81;
 
     public static void main(String[] args) {
-        System.out.println("Введите количество лет: ");
-        Scanner scanner = new Scanner(System.in);
-        int amountOfMonthsInYears = scanner.nextInt() * amountOfMonthsInYear;
-        System.out.println("Введите сумму: ");
-        double summary = scanner.nextDouble();
-        for (int i = 0; i < amountOfMonthsInYears; i++) {
-            summary = summary + ((summary / 100) * percent15);
-        }
-        System.out.println("money= " + summary);
+
     }
 }
---
+++ src/com/p0n0marenk0/Task81.java
@@ -0,0 +1,25 @@
+//task1
+//в банк положили S денег.
+//Какой станет сумма вклада через N лет,
+//если процент 1,5% добавляется к сумме вклада ежемесячно.
+package com.p0n0marenk0;
+
+import java.util.Scanner;
+
+public class Task81 {
+
+    public static final int amountOfMonthsInYear = 12;
+    public static final double percent15 = 1.5;
+
+    public static void main(String[] args) {
+        System.out.println("Введите количество лет: ");
+        Scanner scanner = new Scanner(System.in);
+        int amountOfMonthsInYears = scanner.nextInt() * amountOfMonthsInYear;
+        System.out.println("Введите сумму: ");
+        double summary = scanner.nextDouble();
+        for (int i = 0; i < amountOfMonthsInYears; i++) {
+            summary = summary + ((summary / 100) * percent15);
+        }
+        System.out.printf("money= %.2f", summary);
+    }
+}
