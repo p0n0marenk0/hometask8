7feedf78e7f9738f7d2e6bb5e78c7960ac992efd
Serhii Ponomarenko <ponomarenkoss99@gmail.com>
2021-02-26 20:18:54 +0200

    add task1

--- src/com/p0n0marenk0/Main.java
+++ src/com/p0n0marenk0/Main.java
@@ -1,8 +1,24 @@
+//task1
+//в банк положили S денег.
+//Какой станет сумма вклада через N лет,
+//если процент 1,5% добавляется к сумме вклада ежемесячно.
 package com.p0n0marenk0;
+
+import java.util.Scanner;
 
 public class Main {
 
+    public static final int amountOfMonthsInYear = 12;
+    public static final double percent15 = 1.5;
+
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Введите количество лет: ");
+        Scanner scanner = new Scanner(System.in);
+        int amountOfMonthsInYears = scanner.nextInt() * amountOfMonthsInYear;
+        System.out.println("Введите сумму: ");
+        double summary = scanner.nextDouble();
+        double percent = summary * percent15 / 100;
+        double summ = summary + (percent * amountOfMonthsInYears);
+        System.out.println("money= " + summ);
     }
 }
